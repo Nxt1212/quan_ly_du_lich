867601548a7ea193a5e9ea99f9821b881c5c243f
nptvinh2000 <84074328+nptvinh2000@users.noreply.github.com>
2021-08-27 09:03:19 +0700

    update

--- resources/views/page/common/footer.blade.php
+++ resources/views/page/common/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row mb-5">
             <div class="col-md pt-5">
                 <div class="ftco-footer-widget pt-md-5 mb-4">
-                    <h2 class="ftco-heading-2">About</h2>
+                    <h2 class="ftco-heading-2">Giới thiệu</h2>
                     <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
                         <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
@@ -14,7 +14,7 @@
             </div>
             <div class="col-md pt-5 border-left">
                 <div class="ftco-footer-widget pt-md-5 mb-4 ml-md-5">
-                    <h2 class="ftco-heading-2">Infromation</h2>
+                    <h2 class="ftco-heading-2">Thông tin</h2>
                     <ul class="list-unstyled">
                         <li><a href="#" class="py-2 d-block">Online Enquiry</a></li>
                         <li><a href="#" class="py-2 d-block">General Enquiries</a></li>
@@ -27,7 +27,7 @@
             </div>
             <div class="col-md pt-5 border-left">
                 <div class="ftco-footer-widget pt-md-5 mb-4">
-                    <h2 class="ftco-heading-2">Experience</h2>
+                    <h2 class="ftco-heading-2">Kinh nghiệm</h2>
                     <ul class="list-unstyled">
                         <li><a href="#" class="py-2 d-block">Adventure</a></li>
                         <li><a href="#" class="py-2 d-block">Hotel and Restaurant</a></li>
@@ -40,12 +40,12 @@
             </div>
             <div class="col-md pt-5 border-left">
                 <div class="ftco-footer-widget pt-md-5 mb-4">
-                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <h2 class="ftco-heading-2">Đặt câu hỏi</h2>
                     <div class="block-23 mb-3">
                         <ul>
                             <li><span class="icon fa fa-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
                             <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                            <li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">info@yourdomain.com</span></a></li>
+                            <li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">dulich@yourdomain.com</span></a></li>
                         </ul>
                     </div>
                 </div>
